9450c74c6d2ee30230d7022629f670f82fd5dbdf
qoda <jpbydendyk@gmail.com>
2016-09-19 19:22:23 +0200

    Added model tests

--- formfactory/tests/test_base.py
+++ formfactory/tests/test_base.py
@@ -7,7 +7,23 @@ from formfactory import models
 
 
 def load_fixtures(kls):
-    return kls
+    kls.form_data = {
+        "title": "Form 1",
+        "slug": "form-1"
+    }
+    kls.form = models.Form.objects.create(**kls.form_data)
+
+    for count, field_type in enumerate(models.FIELD_TYPES):
+        setattr(kls, "formfield_data_%s" % count, {
+            "title": "Form Field %s" % count,
+            "position": count,
+            "form": kls.form,
+            "field_type": field_type,
+            "label": "Form Field %s" % count,
+        })
+        setattr(kls, "formfield_%s" % count, models.FormField.objects.create(
+            **getattr(kls, "formfield_data_%s" % count)
+        ))
 
 
 class ModelTestCase(TestCase):
@@ -15,8 +31,29 @@ class ModelTestCase(TestCase):
         load_fixtures(self)
 
     def test_field_constant(self):
-        self.assertIn(("DateTimeField", "DateTimeField"), models.FIELD_TYPES)
 
+        # ensure the field types are populated
+        self.assertIn(("DateTimeField", "DateTimeField"), models.FIELD_TYPES)
+        self.assertIn(("BooleanField", "BooleanField"), models.FIELD_TYPES)
+        self.assertIn(("CharField", "CharField"), models.FIELD_TYPES)
+
+    def test_form(self):
+
+        # ensure the form model has been saved correctly
+        for key, value in self.form_data.items():
+            self.assertEqual(getattr(self.form, key), value)
+
+        # ensure all form fields were saved
+        self.assertEqual(self.form.fields.count(), len(models.FIELD_TYPES))
+
+    def test_formfield(self):
+
+        # ensure the form model has been saved correctly
+        for count in range(len(models.FIELD_TYPES)):
+            formfield_data = getattr(self, "formfield_data_%s" % count)
+            for key, value in formfield_data.items():
+                formfield = getattr(self, "formfield_%s" % count)
+                self.assertEqual(getattr(formfield, key), value)
 
 class AdminTestCase(TestCase):
     def setUp(self):
