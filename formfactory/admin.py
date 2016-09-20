72795f9a01c860249a88bd1d5eda0aecac4dda2b
qoda <jpbydendyk@gmail.com>
2016-09-20 09:48:31 +0200

    Added basic admin classes

--- formfactory/admin.py
+++ formfactory/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+
+from formfactory import forms
+from formfactory.models import FieldChoice, Form, FormField
+
+
+class FieldChoiceModelAdmin(admin.ModelAdmin):
+    form = form.FieldChoiceAdminForm
+    models = FieldChoice
+
+
+class FormFieldInline(admin.StackedInline):
+    form = forms.FormFieldAdminForm
+    model = FormField
+
+
+class FormAdmin(admin.ModelAdmin):
+    form = forms.Form
+    list_display = ["title"]
+    inlines = [FormFieldInline]
+    prepopulated_fields = {"slug": ["title"]}
+
+
+admin.site.register(FieldChoice, FieldChoiceModelAdmin)
+admin.site.register(Form, FormAdmin)
