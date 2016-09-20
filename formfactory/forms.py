9e779f7ba4f6d3f6ab4127de85a8741895c2690e
qoda <jpbydendyk@gmail.com>
2016-09-20 09:48:42 +0200

    Added admin forms

--- formfactory/forms.py
+++ formfactory/forms.py
@@ -1,1 +1,24 @@
 from django import forms
+
+from formfactory.models import FieldChoice, Form, FormField
+
+
+class FormAdminForm(forms.ModelForm):
+    class Meta:
+        model = Form
+        fields = ["title", "slug", "action"]
+
+
+class FieldChoiceAdminForm(forms.ModelForm):
+    class Meta:
+        model = FieldChoice
+        fields = ["label", "value"]
+
+
+class FormFieldAdminForm(forms.ModelForm):
+    class Meta:
+        model = FormField
+        fields = [
+            "title", "position", "form", "field_type", "label", "initial",
+            "label", "required", "disabled", "choices", "additional_validators"
+        ]
