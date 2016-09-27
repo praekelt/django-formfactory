3c856f0af6f7c954a6bd2c743902c5dd321e2e1e
qoda <jpbydendyk@gmail.com>
2016-09-27 16:08:42 +0200

    Added save method to factory form to save to simple data store

--- formfactory/factory.py
+++ formfactory/factory.py
@@ -2,17 +2,19 @@ import uuid
 
 from django import forms
 
+from formfactory.models import FormData, FormDataItems
+
 
 class FormFactory(forms.Form):
     """Builds a form class from defined fields passed to it by the Form model.
     """
-    uuid = forms.UUIDField(
-        initial=unicode(uuid.uuid4()), widget=forms.HiddenInput()
-    )
-
     def __init__(self, *args, **kwargs):
+        self.form_id = kwargs.pop("form_id")
         defined_fields = kwargs.pop("fields")
+
         super(FormFactory, self).__init__(*args, **kwargs)
+
+        self.uuid = unicode(uuid.uuid4())
 
         # Interates over the fields defined in the Form model and sets the
         # appropriate attributes.
@@ -32,6 +34,10 @@ class FormFactory(forms.Form):
                 validators=additional_validators
             )
 
+            # Saves the field model pk to the form field to prevent the need
+            # for another query in the save method.
+            self.fields[field.slug].pk = field.pk
+
             # Adds the field choices and max_length but catches the exception
             # as not all fields allow for these attrs.
             try:
@@ -46,3 +52,18 @@ class FormFactory(forms.Form):
             # Adds widget-specific options to the form field
             widget_attrs = self.fields[field.slug].widget.attrs
             widget_attrs["placeholder"] = field.placeholder
+
+    def save(self, *args, **kwargs):
+        """Saves the data to the store specified on the model.
+        """
+        form_data = FormData.objects.create(
+            uuid=self.uuid,
+            form_id=self.form_id,
+        )
+        for key, value in self.cleaned_data.items():
+            FormDataItems.objects.create(
+                form_data=form_data,
+                form_field_id=self.fields[key].pk,
+                value=value
+            )
+        return form_data
