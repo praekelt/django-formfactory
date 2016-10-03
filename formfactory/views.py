ab506c90f0684cf262436ed9474be89b94728923
qoda <jpbydendyk@gmail.com>
2016-10-03 17:49:30 +0200

    Cleanup

--- formfactory/views.py
+++ formfactory/views.py
@@ -5,9 +5,7 @@ from formfactory.models import Form
 
 class FormDetailView(generic.detail.DetailView):
     model = Form
-    template = "formfactory/form_detail.html"
 
 
 class FormListView(generic.list.ListView):
     model = Form
-    template = "formfactory/form_list.html"
