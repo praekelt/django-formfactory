9eba320bc7e9c2b61568b5033727437b05d81936
qoda <jpbydendyk@gmail.com>
2016-10-03 11:32:57 +0200

    Added form templatetag

--- formfactory/templatetags/formfactory_tags.py
+++ formfactory/templatetags/formfactory_tags.py
@@ -1,3 +1,12 @@
 from django import template
 
+
 register = template.Library()
+
+
+@register.inclusion_tag(
+    "formfactory/inclusion_tags/form_detail.html", takes_context=True
+)
+def render_link(context, obj):
+    context["object"] = obj
+    return context
