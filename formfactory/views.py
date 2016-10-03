658eaf75d0172a41cd915c3100c805bd1b8bc6b3
qoda <jpbydendyk@gmail.com>
2016-10-03 11:47:30 +0200

    Added default views

--- formfactory/views.py
+++ formfactory/views.py
@@ -1,1 +1,13 @@
 from django.views import generic
+
+from formfactory.models import Form
+
+
+class FormDetailView(generic.detail.DetailView):
+    model = Form
+    template = "formfactory/form_detail.html"
+
+
+class FormListView(generic.list.ListView):
+    model = Form
+    template = "formfactory/form_list.html"
