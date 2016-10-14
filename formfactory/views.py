bead6db6033256b3c9686f38fc825355d54c46ff
qoda <jpbydendyk@gmail.com>
2016-10-14 11:07:03 +0200

    Issue #4: Added more control over success urls

--- formfactory/views.py
+++ formfactory/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.views import generic
 
+from formfactory import SETTINGS
 from formfactory.models import Form
 
 
@@ -9,7 +10,7 @@ class FactoryFormView(generic.FormView):
     template_name = "formfactory/form_detail.html"
 
     def form_valid(self, form):
-        form.save()
+        form.save(request=self.request)
         messages.add_message(
             self.request, messages.SUCCESS, self.form_object.success_message
         )
@@ -31,6 +32,11 @@ class FactoryFormView(generic.FormView):
         return self.form_object.as_form()
 
     def get_success_url(self):
+        redirect_url = self.request.GET.get(
+            SETTINGS["redirect-url-param-name"]
+        )
+        if redirect_url:
+            return redirect_url
         return self.request.META.get(
             "HTTP_REFERER", self.request.META.get("PATH_INFO")
         )
