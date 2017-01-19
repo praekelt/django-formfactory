438da968a64f88b58fbae2b5376ebf16970cd5e1
Phala Ramonyai <phalamag@gmail.com>
2017-01-19 12:10:56 +0200

    Remove test action for wizards

--- formfactory/tests/formfactoryapp/actions.py
+++ formfactory/tests/formfactoryapp/actions.py
@@ -5,11 +5,3 @@ from formfactory import actions
 def dummy_action(form_instance):
     return True
 
-
-@actions.register
-def store_form_data(form_dict, **kwargs):
-    """
-    Test wizard action to save each form's data
-    """
-    for key, form in form_dict.items():
-        form.save()
