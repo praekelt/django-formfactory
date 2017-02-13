7ba0a6e3a5788419029381c40270da6737dd2e87
qoda <jpbydendyk@gmail.com>
2017-02-13 20:56:13 +0200

    Added action to upload files

--- formfactory/actions.py
+++ formfactory/actions.py
@@ -1,3 +1,7 @@
+import os
+
+from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.mail import send_mail
 from django.contrib import auth
 
@@ -104,3 +108,33 @@ def login(form_instance, **kwargs):
     )
     if user is not None:
         auth.login(request, user)
+
+
+@register
+def file_upload(form_instance, **kwargs):
+    """An action which uploads all files to a specific location.
+    """
+    cleaned_data = form_instance.cleaned_data
+
+    file_objects = [
+        f for f in cleaned_data.values() if isinstance(f, InMemoryUploadedFile)
+    ]
+
+    print "file_objects", file_objects
+
+    try:
+        upload_path = cleaned_data.pop(kwargs["upload_path_field"])
+    except KeyError:
+        raise exceptions.MissingActionParam("file_upload", "upload_path_field")
+
+    full_upload_path = os.path.join(settings.MEDIA_ROOT, upload_path)
+
+    # Creates the dir path if it does not already exist
+    if not os.path.exists(full_upload_path):
+        os.makedirs(full_upload_path)
+
+    for file_object in file_objects:
+        file_path = os.path.join(full_upload_path, file_object.name)
+        with open(file_path, "wb+") as destination:
+            for chunk in file_object.chunks():
+                destination.write(chunk)
