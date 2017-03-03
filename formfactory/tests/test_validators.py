11b38d4f643a77d0dea0bb1289db2a91f2918d85
Phala Ramonyai <phalamag@gmail.com>
2017-03-03 10:31:25 +0200

    Add test for validator

--- formfactory/tests/test_validators.py
+++ formfactory/tests/test_validators.py
@@ -1,6 +1,7 @@
+from django.forms import ValidationError
 from django.test import TestCase
 
-from formfactory import validators
+from formfactory import models, validators
 from formfactory.tests.test_base import load_fixtures
 
 
@@ -22,8 +23,37 @@ class ValidatorTestCase(TestCase):
             self.dummy_validator, validators.get_registered_validators()
         )
 
-    def test_action(self):
+    def test_form_field_validator(self):
         validator = validators.get_registered_validators()[
             self.dummy_validator
         ]
         self.assertTrue(validator(2))
+
+    def test_form_field_validation(self):
+        field = models.FormField.objects.create(
+            title="Number",
+            slug="number",
+            field_type="IntegerField",
+            additional_validators=self.dummy_validator
+        )
+        group = models.FormFieldGroup.objects.create(
+            title="Field Group 3",
+            show_title=False
+        )
+        models.FieldGroupThrough.objects.create(
+            field=field, field_group=group, order=0
+        )
+        form = models.Form.objects.create(
+            title="Form 1",
+            slug="slug-1"
+        )
+        models.FieldGroupFormThrough.objects.create(
+            form=form,
+            field_group=group,
+            order=0
+        )
+        bound_form = form.as_form(data={"number": 3})
+        self.assertRaises(
+            ValidationError,
+            lambda: bound_form.fields["number"].run_validators(3)
+        )
