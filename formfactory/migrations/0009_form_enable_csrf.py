270c8bed0958a1f5d6d0bfd5b24da39f785b6087
Phala Ramonyai <phalamag@gmail.com>
2017-03-13 18:27:51 +0200

    Fixed order of migrations

--- formfactory/migrations/0009_form_enable_csrf.py
+++ formfactory/migrations/0009_form_enable_csrf.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('formfactory', '0005_added_enum_generic_relation'),
+        ('formfactory', '0008_custom_field_error_messages'),
     ]
 
     operations = [
