c9182994385005546fcac97e99873d00a85cf6b5
Phala Ramonyai <phalamag@gmail.com>
2017-03-21 18:22:56 +0200

    Code cleanup

--- formfactory/fields.py
+++ formfactory/fields.py
@@ -1,13 +1,22 @@
-from django.forms.fields import Field
 from django.apps import apps
+from django.forms.fields import Field
 from django.utils.module_loading import import_module
 
-from formfactory import _registry
+from formfactory import _registry, SETTINGS
+
+# Keys for fields registered in _registry["fields"] are suffixed.
+# This constant is used for convenience in  getting the django
+# built-in fields.
+DJANGO_FIELDS_MODULE = "django.forms.fields"
 
 
 def auto_discover():
-    """Perform discovery of custom fields
+    """Perform discovery of custom fields, starting with the
+    built-in django fields.
     """
+    module = import_module(DJANGO_FIELDS_MODULE)
+    get_form_fields(module)
+
     for app in apps.get_app_configs():
         try:
             module = import_module("%s.formfactoryapp.%s" % (app.name, "fields"))
@@ -15,17 +24,42 @@ def auto_discover():
         except ImportError:
             pass
 
+    # remove blacklisted django built-in fields
+    remove_excluded_fields()
+
+
+def formatted_field_key(field_name, suffix):
+    """Returns a key in the format it will be stored in
+     _registry["fields"], i.e. 'field_name-suffix'
+    """
+    return "{}-{}".format(field_name, suffix)
+
 
 def get_form_fields(module):
     """Get all form fields defined in this module.
      Form fields should be a subclass of django.forms.fields.Field
     """
+    # TODO: provide a way to limit which django form fields are available in the admin.
+
     for name in dir(module):
         try:
             if issubclass(getattr(module, name), Field):
-                _registry["fields"][name] = getattr(module, name)
+                field_class = getattr(module, name)
+                key = formatted_field_key(
+                    field_class.__name__, field_class.__module__
+                )
+                _registry["fields"][key] = field_class
         except TypeError:
             pass
+
+
+def remove_excluded_fields():
+    """Remove fields listed in settings.FORMFACTORY['excluded_fields']
+    """
+    for field_name in SETTINGS["excluded_fields"]:
+        _registry["fields"].pop(
+            formatted_field_key(field_name, DJANGO_FIELDS_MODULE)
+        )
 
 
 def get_registered_fields():
