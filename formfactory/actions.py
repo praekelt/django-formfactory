34daaf2d146da03d84b247688873b9ec18c204f0
Phala Ramonyai <phalamag@gmail.com>
2017-06-02 09:53:59 +0200

    Remove unused imports

--- formfactory/actions.py
+++ formfactory/actions.py
@@ -1,11 +1,9 @@
 import os
 
-from django import forms
 from django.conf import settings
 from django.contrib import auth
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.mail import send_mail
-from django.utils.translation import ugettext_lazy as _
 
 from formfactory import _registry, exceptions
 from formfactory.utils import (
@@ -112,7 +110,6 @@ def login(form_instance, **kwargs):
     )
     if user is not None:
         auth.login(request, user)
-
 
 
 @register
