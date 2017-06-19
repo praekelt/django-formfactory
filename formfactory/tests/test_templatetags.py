39b617b7cacf1ed6ef53e3b4dcc931674e858c37
AltusBarry <AltusBarry@users.noreply.github.com>
2017-06-19 16:49:00 +0200

    Updated tests

--- formfactory/tests/test_templatetags.py
+++ formfactory/tests/test_templatetags.py
@@ -1,4 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.template import Context, Template
+from django.test import LiveServerTestCase, Client
 from django.test import TestCase
 
 from formfactory.tests.test_base import load_fixtures
@@ -9,18 +11,24 @@ class TemplateTagsTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         load_fixtures(cls)
-
-    def test_get_form_from_context(self):
-        template = Template(
-            "{% load formfactory_tags %}{% render_form form_object %}"
-        )
-        context = Context({"form_object": self.loginform})
-        result = template.render(context)
-        self.failUnless("login-form-form_id" in result)
+        cls.client = Client()
+        super(TemplateTagsTestCase, cls).setUpTestData()
 
     def test_get_form_by_slug(self):
-        template = Template(
-            "{% load formfactory_tags %}{% render_form 'login-form' %}"
+        response = self.client.get(
+            reverse(
+                "render_tag"
+            )
         )
-        result = template.render(Context())
-        self.failUnless("login-form-form_id" in result)
+        self.failUnless("login-form-form_id" in response.content)
+
+    def test_default_detail_template(self):
+        # Detail view already makes use of the render_form tag and passes an
+        # object.
+        response = self.client.get(
+            reverse(
+                "formfactory:form-detail",
+                kwargs={"slug": "login-form"}
+            )
+        )
+        self.failUnless("login-form-form_id" in response.content)
