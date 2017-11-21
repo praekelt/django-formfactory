83d6206c6087e4e5f91c05cc1606d8a4fda4b5c1
AltusBarry <AltusBarry@users.noreply.github.com>
2017-11-21 12:13:25 +0200

    Actually add test setting and requirement

---
+++ formfactory/tests/settings/2.py
@@ -0,0 +1,61 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "test111.db"
+    }
+}
+
+ROOT_URLCONF = "formfactory.tests.urls"
+
+INSTALLED_APPS = [
+    "formfactory",
+    "formfactory.tests",
+    "formtools",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "simplemde"
+]
+
+MIDDLEWARE_CLASSES = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+SITE_ID = 1
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+SECRET_KEY = "SECRET_KEY"
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
